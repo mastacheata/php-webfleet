c0b94c6f3f2a93744425a6b5b362f11d668ff6b3
smiley <smiley@chillerlan.net>
2015-12-19 21:50:35 +0100

    :octocat:

---
+++ src/WebfleetException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class WebfleetException
+ *
+ * @filesource   WebfleetException.php
+ * @created      13.12.2015
+ * @package      TomTom\Telematics
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace TomTom\Telematics;
+
+use Exception;
+
+/**
+ * placeholder
+ */
+class WebfleetException extends Exception{
+
+}
